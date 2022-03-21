a42a91aa046d600e534cdc5ae9c3964720ff89c0
Cael dos Santos <cgoncalves@id.uff.br>
2022-03-21 20:41:57 -0300

    Criada base de relações da turma e disciplina

--- app/models/college_class.rb
+++ app/models/college_class.rb
@@ -1,2 +1,7 @@
-class CollegeClass < ApplicationRecord
+class CollegeClass < ApplicationRecord #Turma
+    belongs_to :discipline
+    belongs_to :department
+    belongs_to :teacher, class_name: 'User', foreign_key: 'user_id'
+    has_many :class_enrollments
+    has_many :users, through: :class_enrollments
 end
--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,2 +1,4 @@
 class Discipline < ApplicationRecord
+    belongs_to :department
+    has_many :college_classes, dependent: :destroy #Caso uma matéria seja deletada, todas as suas turmas também serão
 end
