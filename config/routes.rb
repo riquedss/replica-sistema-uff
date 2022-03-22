a03ba1c81e2870b48b12ec70ff3948f008e0cc7f
Henrique Santana <hsantana.sousass@gmail.com>
2022-03-22 12:58:46 -0300

    add routes para as funções do CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/departments', to: "departments#index"
+  get '/department/:id', to: "departments#show"
+  post '/department', to: "departments#create"
+  put '/department/:id', to: "departments#update"
+  delete '/department/:id', to: "departments#delete"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
