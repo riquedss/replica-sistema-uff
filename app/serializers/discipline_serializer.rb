97c230aa5f417888b6a773040c2dc447f59b4bd5
Cael dos Santos <cgoncalves@id.uff.com>
2022-03-23 20:45:24 -0300

    Adicionado serizalizer da disciplina

---
+++ app/serializers/discipline_serializer.rb
@@ -0,0 +1,11 @@
+class DisciplineSerializer < ActiveModel::Serializer
+  attributes :id, :name, :hours, :knowledge_area
+
+  has_many :college_classes 
+  has_many :teachers, through: :college_classes
+    
+  #Pre-requisitos da matéria
+  has_many :requirements, through: :requisits
+  #Matérias que possuem esta matéria como pré-requisito
+  has_many :targets, through: :follow_ups
+end
