7a3b9e05d97f69614e0daeaa4fe9545dd2afdd43
Cael dos Santos <cgoncalves@id.uff.com>
2022-03-24 11:44:57 -0300

    Pequenos ajustes nas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -13,53 +13,52 @@ Rails.application.routes.draw do
 
   #LOGIN
 
-  post 'login/', to: 'auth#login'
+  post '/login/', to: 'auth#login'
 
   #DIRECTOR
 
-  get 'director/', to: 'director#index'
-  post 'director/', to: 'director#create'
-  patch 'director/', to: 'director#update'
+  get '/director/', to: 'director#index'
+  post '/director/', to: 'director#create'
+  patch '/director/', to: 'director#update'
 
   #STUDENTS
 
-  get 'students/', to: 'students#index'
-  get 'students/:id', to: 'students#show'
-  post 'students/', to: 'students#create'
-  patch 'students/:id', to: 'students#update'
-  delete 'students/:id', to: 'students#destroy'
+  get '/students/', to: 'students#index'
+  get '/students/:id', to: 'students#show'
+  post '/students/:id', to: 'students#update'
+  delete '/students/:id', to: 'students#destroy'
 
   #PROFESSORS
 
-  get 'professors/', to: 'professors#index'
-  get 'professors/:id', to: 'professors#show'
-  post 'professors/', to: 'professors#create'
-  patch 'professors/:id', to: 'professors#update'
-  delete 'professors/:id', to: 'professors#destroy'
+  get '/professors/', to: 'professors#index'
+  get '/professors/:id', to: 'professors#show'
+  post '/professors/', to: 'professors#create'
+  patch '/professors/:id', to: 'professors#update'
+  delete '/professors/:id', to: 'professors#destroy'
 
   #DEPARTMENT COORDINATORS
 
-  get 'deptcoords/', to: 'department_coordinators#index'
-  get 'deptcoords/:id', to: 'department_coordinators#show'
-  post 'deptcoords/', to: 'department_coordinators#create'
-  patch 'deptcoords/:id', to: 'department_coordinators#update'
-  delete 'deptcoords/:id', to: 'department_coordinators#destroy'
+  get '/deptcoords/', to: 'department_coordinators#index'
+  get '/deptcoords/:id', to: 'department_coordinators#show'
+  post '/deptcoords/', to: 'department_coordinators#create'
+  patch '/deptcoords/:id', to: 'department_coordinators#update'
+  delete '/deptcoords/:id', to: 'department_coordinators#destroy'
 
   #COURSE COORDINATORS
 
-  get 'coursecoords/', to: 'course_coordinators#index'
-  get 'coursecoords/:id', to: 'course_coordinators#show'
-  post 'coursecoords/', to: 'course_coordinators#create'
-  patch 'coursecoords/:id', to: 'course_coordinators#update'
-  delete 'coursecoords/:id', to: 'course_coordinators#destroy'
+  get '/coursecoords/', to: 'course_coordinators#index'
+  get '/coursecoords/:id', to: 'course_coordinators#show'
+  post '/coursecoords/', to: 'course_coordinators#create'
+  patch '/coursecoords/:id', to: 'course_coordinators#update'
+  delete '/coursecoords/:id', to: 'course_coordinators#destroy'
 
   #USERS
 
-  get 'users/', to: 'users#index'
-  get 'users/:id', to: 'users#show'
-  post 'users/', to: 'users#create'
-  patch 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
+  get '/users/', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users/', to: 'users#create'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/departments', to: 'departments#index'
