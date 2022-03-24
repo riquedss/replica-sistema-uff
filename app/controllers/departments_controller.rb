4181a90739e697c418f17894983c2a468a2fdaf7
Henrique Santana <hsantana.sousass@gmail.com>
2022-03-23 23:33:06 -0300

    add funcoes que verificam as permissoes para a utilizacao das routes

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,7 +1,9 @@
 class DepartmentsController < ApplicationController
-  before_action :set_department, only: [:show ,:update, :delete]
-  before_action :verify_authenticated_user_director, only: [:create, :delete]
-  before_action :verify_authenticated_user_department_coordinator, only: [:show ,:update, :delete]
+  before_action :verify_authenticated, only: [:index, :show, :create, :update, :delete]
+  before_action :set_department, only: [:show, :update, :delete]
+  before_action :verify_director_authenticated, only: [:create, :delete]
+  before_action :verify_dept_coord_authenticated, only: [:show, :index]
+  before_action :is_derpt_coord_of_this_derpt_or_director, only: [:update]
 
   def index
     @departments = Department.all
@@ -31,17 +33,23 @@ class DepartmentsController < ApplicationController
   end
 
   def delete
-    @department.destroy
+    def delete
+      if @department.destroy
+        render json: { message: "deleted" }, status: :ok
+      else
+        render json: @period.errors, status: :not_found
+      end
+    end
+  end
+  
+  def set_department
+    @department = Department.find(params[:id])
   end
 
   private
-
   def params_department
     params.require(:department).permit(:name, :knowledge_area, :campus, :user_id)
   end
 
-  def set_department
-    @department = Department.find(params[:id])
-  end
 end
 
